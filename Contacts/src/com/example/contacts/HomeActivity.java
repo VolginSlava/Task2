bc6eb87c0454429bc73c62d7cee9cb5df5e2bfde
unknown <volgin@clarksville.mercury.office>
2014-10-20 12:16:45 +0400

    Permission dialog fragment implemented

--- Contacts/src/com/example/contacts/HomeActivity.java
+++ Contacts/src/com/example/contacts/HomeActivity.java
@@ -10,14 +10,46 @@ import com.example.contacts.tools.Logging;
 
 public class HomeActivity extends ActionBarActivity {
 
+	private static final String READ_CONTACTS_PERMISSION_ACQUIRED_KEY = "readContactsPermissionAcquired";
+	private static final boolean READ_CONTACTS_PERMISSION_ACQUIRED_DEFAULT_VALUE = false;
+
+
+	private boolean readContactsPermissionAcquired;
+
 	private DialogUtils dialogUtils = new DialogUtils();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		Logging.logEntrance(ACTIVITY_SERVICE);
 
-		dialogUtils.addPermissionDialog();
+		if (savedInstanceState == null) {
+			savedInstanceState = new Bundle();
+		}
+
+		readContactsPermissionAcquired = savedInstanceState.getBoolean(READ_CONTACTS_PERMISSION_ACQUIRED_KEY, READ_CONTACTS_PERMISSION_ACQUIRED_DEFAULT_VALUE);
+
+		if (!readContactsPermissionAcquired) {
+			askForReadContactsPermission();
+		}
+
+	}
+
+	private void askForReadContactsPermission() {
+		Logging.logEntrance(ACTIVITY_SERVICE);
+		PermissionDialogFragment dialog = dialogUtils.getPermissionDialogFragment();
+		dialog.setListener(dialogUtils);
+		if (!dialog.isAdded()) {
+			dialog.show(getFragmentManager(), DialogUtils.PERMISSION_DIALOG_FRAGMENT_TAG);
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Logging.logEntrance(ACTIVITY_SERVICE);
+		outState.putBoolean(READ_CONTACTS_PERMISSION_ACQUIRED_KEY, readContactsPermissionAcquired);
 	}
 
 	@Override
@@ -41,49 +73,69 @@ public class HomeActivity extends ActionBarActivity {
 
 	private class DialogUtils implements Listener {
 
-		private static final String PERMISSION_DIALOG_FRAGMENT_TAG = "progressDialogFragment";
+		private static final String DIALOG_UTILS = "DialogUtils";
+		private static final String PERMISSION_DIALOG_FRAGMENT_TAG = "permissionDialogFragment";
 
-		private PermissionDialogFragment permissionDialogFragment;
 
-		private PermissionDialogFragment getFragment() {
-			Logging.logEntrance(ACTIVITY_SERVICE);
-			PermissionDialogFragment fragment = (PermissionDialogFragment) getFragmentManager().findFragmentByTag(PERMISSION_DIALOG_FRAGMENT_TAG);
-			if (fragment == null) {
-				if (permissionDialogFragment == null) {
-					permissionDialogFragment = new PermissionDialogFragment();
-				}
-				fragment = permissionDialogFragment;
+		private PermissionDialogFragment getPermissionDialogFragment() {
+			Logging.logEntrance(DIALOG_UTILS);
+			PermissionDialogFragment dialog = (PermissionDialogFragment) getFragmentManager().findFragmentByTag(PERMISSION_DIALOG_FRAGMENT_TAG);
+			if (dialog == null) {
+				dialog = PermissionDialogFragment.newInstance();
 			}
-			return fragment;
+			return dialog;
 		}
 
-		private void addPermissionDialog() {
-			boolean res = getFragmentManager().executePendingTransactions();
-			Logging.logEntrance(ACTIVITY_SERVICE, "executePendingTransactions: " + res);
-
-			if (!getFragment().isAdded()) {
-				getFragmentManager().beginTransaction().add(getFragment(), PERMISSION_DIALOG_FRAGMENT_TAG).commit();
-			}
+		public void onPositiveClick() {
+			Logging.logEntrance(DIALOG_UTILS);
+			readContactsPermissionAcquired = true;
 		}
 
-		private void removePermissionDialog() {
-			boolean res = getFragmentManager().executePendingTransactions();
-			Logging.logEntrance(ACTIVITY_SERVICE, "executePendingTransactions: " + res);
 
-			if (getFragment().isAdded()) {
-				getFragmentManager().beginTransaction().remove(getFragment()).commit();
-			}
-		}
 
-		private void addListener() {
-			Logging.logEntrance(ACTIVITY_SERVICE);
-			getFragment().setListener(DialogUtils.this);
-		}
 
-		private void removeListener() {
-			Logging.logEntrance(ACTIVITY_SERVICE);
-			getFragment().setListener(null);
-		}
+		// @Deprecated
+		// private PermissionDialogFragment getFragment() {
+		// Logging.logEntrance(ACTIVITY_SERVICE);
+		// PermissionDialogFragment fragment = (PermissionDialogFragment) getFragmentManager().findFragmentByTag(PERMISSION_DIALOG_FRAGMENT_TAG);
+		// if (fragment == null) {
+		// if (permissionDialogFragment == null) {
+		// permissionDialogFragment = PermissionDialogFragment.newInstance();
+		// }
+		// fragment = permissionDialogFragment;
+		// }
+		// return fragment;
+		// }
+
+
+		// private void addPermissionDialog() {
+		// boolean res = getFragmentManager().executePendingTransactions();
+		// Logging.logEntrance(ACTIVITY_SERVICE, "executePendingTransactions: " + res);
+		//
+		// if (!getFragment().isAdded()) {
+		// getFragmentManager().beginTransaction().add(getFragment(), PERMISSION_DIALOG_FRAGMENT_TAG).commit();
+		// }
+		// }
+		//
+		// private void removePermissionDialog() {
+		// boolean res = getFragmentManager().executePendingTransactions();
+		// Logging.logEntrance(ACTIVITY_SERVICE, "executePendingTransactions: " + res);
+		//
+		// if (getFragment().isAdded()) {
+		// getFragmentManager().beginTransaction().remove(getFragment()).commit();
+		// }
+		// }
+
+		// private void addListener() {
+		// Logging.logEntrance(ACTIVITY_SERVICE);
+		// getFragment().setListener(DialogUtils.this);
+		// }
+		//
+		// private void removeListener() {
+		// Logging.logEntrance(ACTIVITY_SERVICE);
+		// getFragment().setListener(null);
+		// }
+
 
 		// @Override
 		// public void onCancel() {
--- Contacts/src/com/example/contacts/PermissionDialogFragment.java
+++ Contacts/src/com/example/contacts/PermissionDialogFragment.java
@@ -1,24 +1,106 @@
 package com.example.contacts;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnKeyListener;
 import android.os.Bundle;
+import android.view.KeyEvent;
+
+import com.example.contacts.tools.Logging;
 
 public class PermissionDialogFragment extends DialogFragment {
-	public static interface Listener {
-
-	}
 
 	private static final String DIALOG_FRAGMENT = "PermissionDialog";
+	private static final String TITLE_KEY = "title";
+	private static final String MESSAGE_KEY = "message";
+
+	public static PermissionDialogFragment newInstance() {
+		PermissionDialogFragment fragment = new PermissionDialogFragment();
+
+		Bundle args = new Bundle();
+		args.putInt(TITLE_KEY, R.string.permission_dialog_title);
+		args.putInt(MESSAGE_KEY, R.string.permission_dialog_message);
+		fragment.setArguments(args);
+
+		return fragment;
+	}
+
 
 	private Listener listener;
 
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		return super.onCreateDialog(savedInstanceState);
+		Logging.logEntrance(DIALOG_FRAGMENT);
+		int titleId = getArguments().getInt(TITLE_KEY);
+		int messageId = getArguments().getInt(MESSAGE_KEY);
+
+		Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setTitle(titleId)
+				.setMessage(messageId)
+				.setCancelable(false);
+
+		builder.setPositiveButton(R.string.permission_dialog_button_ok, getPositiveClickListener());
+		builder.setNegativeButton(R.string.permission_dialog_button_cancel, getNegativeClickListener());
+		builder.setOnKeyListener(getBackKeyListener());
+
+		return builder.create();
+	}
+
+	private OnClickListener getPositiveClickListener() {
+		return new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				doPositiveClick();
+			}
+		};
+	}
+
+	private OnClickListener getNegativeClickListener() {
+		return new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				doNegativeClick();
+			}
+		};
+	}
+
+	private OnKeyListener getBackKeyListener() {
+		return new DialogInterface.OnKeyListener() {
+			public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+				// Logging.logEntrance(DIALOG_FRAGMENT, String.format("keyCode: %d, isBackPressed: %b, KeyEvent: %s", keyCode, keyCode == KeyEvent.KEYCODE_BACK, event));
+				if (keyCode == KeyEvent.KEYCODE_BACK) {
+					if (event.getAction() == KeyEvent.ACTION_UP) {
+						Logging.logEntrance(DIALOG_FRAGMENT, "Back key pressed");
+						doNegativeClick();
+						return true;
+					}
+				}
+				return false;
+			}
+		};
+	}
+
+	protected void doNegativeClick() {
+		Logging.logEntrance(DIALOG_FRAGMENT);
+		getActivity().finish();
+	}
+
+	protected void doPositiveClick() {
+		Logging.logEntrance(DIALOG_FRAGMENT);
+		if (listener != null) {
+			listener.onPositiveClick();
+		}
 	}
 
 	public void setListener(Listener listener) {
+		Logging.logEntrance(DIALOG_FRAGMENT);
 		this.listener = listener;
+	}
+
+	public static interface Listener {
+		void onPositiveClick();
 	}
 }
